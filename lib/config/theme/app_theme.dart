ba70b4e8d637c5a8216aec2664bba0d5137430c2
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-05-17 20:30:53 -0500

    feat: Modify App Theme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
-  ThemeData getTheme() => ThemeData(colorSchemeSeed: const Color(0xFF121212));
+  ThemeData getTheme() {
+    const seedColor = Color(0xFFB71C1C);
+    const backgroundColor = Color(0xFF121212);
+
+    return ThemeData(
+      colorSchemeSeed: seedColor,
+      scaffoldBackgroundColor: backgroundColor,
+      textTheme: GoogleFonts.montserratTextTheme().copyWith(
+        bodyLarge: GoogleFonts.montserrat(color: Colors.grey[200]),
+        bodyMedium: GoogleFonts.montserrat(color: Colors.grey[200]),
+        titleLarge: GoogleFonts.montserrat(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+          color: Colors.grey[200],
+        ),
+        titleMedium: GoogleFonts.montserrat(color: seedColor),
+        headlineSmall: GoogleFonts.montserrat(color: Colors.grey[200]),
+        labelLarge: GoogleFonts.montserrat(color: Colors.grey[200]),
+        titleSmall: GoogleFonts.montserrat(color: Colors.grey[200]),
+        bodySmall: GoogleFonts.montserrat(color: Colors.grey[200]),
+      ),
+    );
+  }
 }
